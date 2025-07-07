77401b42e1f8f26bf167ddee275d98f8acb7812f
cyckevin <418064738@qq.com>
2025-07-07 10:44:21 +0800

    1

--- 0707/batch_planner.py
+++ 0707/batch_planner.py
@@ -41,46 +41,28 @@ def get_prev_batch(date: datetime.date, batch_no: int, n_batches: int):
     batch_id = f"{prev_date.strftime('%Y%m%d')}_{prev_batch_no:02d}"
     return prev_date, prev_batch_no, batch_id
 
-def group_files(source_folder):
-    """
-    遍历source_folder，配对同名json和wav文件，返回[(json_path, wav_path), ...]
-    """
-    files = os.listdir(source_folder)
-    json_files = {os.path.splitext(f)[0]: os.path.join(source_folder, f) for f in files if f.endswith('.json')}
-    wav_files = {os.path.splitext(f)[0]: os.path.join(source_folder, f) for f in files if f.endswith('.wav')}
-    groups = []
-    for name in json_files:
-        if name in wav_files:
-            groups.append((json_files[name], wav_files[name]))
-    return groups
 
-def split_batches(file_groups, max_zip_size, max_zip_count):
+def get_next_batch_id(current_batch_id: str, n_batches: int) -> str:
     """
-    按zip最大体积分组，返回batches, extra_files
-    batches: [[(json, wav), ...], ...]
-    extra_files: [(json, wav), ...]
+    输入当前batch_id（如'20240708_03'）和n_batches，返回下一个batch_id，支持跨日期。
     """
-    batches = []
-    current_batch = []
-    current_size = 0
-    for group in file_groups:
-        size = os.path.getsize(group[0]) + os.path.getsize(group[1])
-        if current_size + size > max_zip_size and current_batch:
-            batches.append(current_batch)
-            current_batch = []
-            current_size = 0
-        current_batch.append(group)
-        current_size += size
-    if current_batch:
-        batches.append(current_batch)
-    # 超出最大zip数的文件组
-    if len(batches) > max_zip_count:
-        extra_batches = batches[max_zip_count:]
-        batches = batches[:max_zip_count]
-        extra_files = [item for batch in extra_batches for item in batch]
-    else:
-        extra_files = []
-    return batches, extra_files
+    import datetime
+    date_str, batch_no_str = current_batch_id.split('_')
+    date = datetime.datetime.strptime(date_str, '%Y%m%d').date()
+    batch_no = int(batch_no_str)
+    next_date, next_batch_no, next_batch_id = get_next_batch(date, batch_no, n_batches)
+    return next_batch_id
+
+def get_prev_batch_id(current_batch_id: str, n_batches: int) -> str:
+    """
+    输入当前batch_id（如'20240708_03'）和n_batches，返回上一个batch_id，支持跨日期。
+    """
+    import datetime
+    date_str, batch_no_str = current_batch_id.split('_')
+    date = datetime.datetime.strptime(date_str, '%Y%m%d').date()
+    batch_no = int(batch_no_str)
+    prev_date, prev_batch_no, prev_batch_id = get_prev_batch(date, batch_no, n_batches)
+    return prev_batch_id
 
 if __name__ == "__main__":
     # 支持命令行参数传入batch数
